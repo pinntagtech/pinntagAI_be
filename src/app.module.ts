6aa8a08e7dc1486627fb3935b6f808a10d6699ef
RajanBakshi238 <rajan.bakshi238@gmail.com>
2025-03-05 00:02:09 +0530

    admin module seprated

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,6 +35,7 @@ import { Token, TokenSchema } from './auth/models/token.model';
 import { CronModule } from './cron/cron.module';
 import { SmsModule } from './sms/sms.module';
 import { Otp, OtpSchema } from './auth/models/otp.model';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
@@ -78,6 +79,7 @@ import { Otp, OtpSchema } from './auth/models/otp.model';
     InAppPurchaseModule,
     CronModule,
     SmsModule,
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService, Logger, SeederService],
