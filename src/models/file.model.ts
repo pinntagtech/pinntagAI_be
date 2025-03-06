8660324201d072ed84476947a9f13e5817a92cf3
apple <apple@MacBook-Pro.local>
2025-03-06 12:15:45 +0530

    file model

---
+++ src/models/file.model.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+export type fileDocument = File & Document;
+@Schema({ timestamps: true })
+export class File {
+  @Prop()
+  gallery: mongoose.Types.ObjectId;
+  @Prop()
+  event: mongoose.Types.ObjectId;
+  @Prop({ required: true })
+  url: string;
+  @Prop()
+  isCoverImage: boolean;
+}
+
+export const FileSchema = SchemaFactory.createForClass(File);
