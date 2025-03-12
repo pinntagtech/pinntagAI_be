87f8d11285dcaf7e5cafb5c21b0fa8bf89232940
apple <apple@MacBook-Pro.local>
2025-03-12 16:16:26 +0530

    file upload

--- src/drive/drive.controller.ts
+++ src/drive/drive.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('drive')
 export class DriveController {
   constructor() {}
+
+   @Post('fileUpload')
+    @UseGuards(JwtGuard)
+    @UseInterceptors(
+      FileInterceptor(
+        'file',
+        // , {
+        //   dest: './uploads',
+        //   fileFilter: imageFileFilter,
+        //   storage: diskStorage({
+        //     destination: './uploads',
+        //     filename: editFileName,
+        //   }),
+        // }
+        {
+          // storage: memoryStorage(),
+          fileFilter: (req, file, cb) => {
+            if (
+              allowedMimeTypes.images.includes(file.mimetype) ||
+              allowedMimeTypes.videos.includes(file.mimetype)
+            ) {
+              cb(null, true);
+            } else {
+              cb(
+                new BadRequestException(
+                  'Invalid file type. Only images and videos are allowed.',
+                ),
+                false,
+              );
+            }
+          },
+          limits: { fileSize: 100 * 1024 * 1024 }, // ✅ Set file size limit to 100MB
+        },
+      ),
+    )
 
 }
