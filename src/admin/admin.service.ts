89b75cb5703e98e87f1dae375e333cadb7caa529
Rahul Sharma <rahulsharma@Rahuls-MacBook-Pro.local>
2025-03-23 13:58:43 +0530

    code push

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -717,11 +717,11 @@ export class AdminService {
     }
     data.password = await bcrypt.hash(data.password, 10);
     if (data.role) {
-      const role = await this.roleModel.findOne({ name: data.role });
+      const role = await this.roleModel.findById(data.role);
       if (!role) {
         return {
           success: false,
-          message: 'Role not found with the name provided.',
+          message: 'Role not found with the id provided.',
         };
       }
       data.role = role._id;
