05f547c0493a29ae28bb824ecfa2148e81aee824
apple <apple@MacBook-Pro.local>
2025-04-03 22:57:08 +0530

    downline user

--- src/business/business.service.ts
+++ src/business/business.service.ts
@@ -1357,7 +1357,7 @@ export class BusinessService {
       return {
         success: true,
         message: 'Business User Created Successfully!',
-        data: updatedUser,
+        data: updatedUser[0],
       };
     } catch (error) {
       return {
