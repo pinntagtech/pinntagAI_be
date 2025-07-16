7a25323ef4008e51bf5279ca9580e8c21ee01d26
Rahul Sharma <153061855+gabrewNS@users.noreply.github.com>
2025-07-17 01:53:11 +0530

    dynamic link

--- src/event/event.service2.ts
+++ src/event/event.service2.ts
@@ -397,7 +397,9 @@ export class EventService2 {
     ).toISOString();
 
     for (let i = 0; i < eventInfo.eventSchedule.length; i++) {
-      if (!requiredSchedule) {
+      if (requiredSchedule) {
+        break;
+      } else {
         const schedule = await this.scheduleModel.findOne({
           _id: eventInfo.eventSchedule[i],
         });
@@ -411,23 +413,33 @@ export class EventService2 {
                 console.log('Start time:', durations[j].startTime);
                 requiredSchedule = durations[j].startTime;
                 break;
-              } else {
-                return {
-                  success: false,
-                  message: 'No upcoming schedule found for this event',
-                };
               }
+              // else {
+              //   // return {
+              //   //   success: false,
+              //   //   message: 'No upcoming schedule found for this event',
+              //   // };
+              // }
             }
-          } else {
-            return {
-              success: false,
-              message: 'No upcoming schedule found for this event',
-            };
           }
+          // else {
+          //   return {
+          //     success: false,
+          //     message: 'No upcoming schedule found for this event',
+          //   };
+          // }
         }
       }
     }
-    console.log('Required schedule:', requiredSchedule);
+    if (!requiredSchedule) {
+      console.log('No upcoming schedule found');
+      return {
+        success: false,
+        message: 'No upcoming schedule found for this event',
+      };
+    } else {
+      console.log('Required schedule:', requiredSchedule);
+    }
     if (eventInfo.eventSchedule.length == 1) {
       eventDescription = getStringDateTzWithTime(new Date(requiredSchedule));
     } else {
@@ -6724,7 +6736,7 @@ export class EventService2 {
   //           $addToSet: { eventSchedule: schedule3._id },
   //         },
   //       );
-      
+
   //     }
   //     return {
   //       success: true,
