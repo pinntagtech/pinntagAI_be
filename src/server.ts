b011e40687e0ef6ff760d59c4c5acdbe2e1f2f1c
Xperience <xperience@JATTs-MacBook-Pro.local>
2025-09-12 14:55:35 +0530

    Delete all files

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app.ts";
-
-
-const PORT = process.env.PORT || 5000;
-console.log(process.env.PORT, "PORT");
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
