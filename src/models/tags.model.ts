ada0200bd1253170ad19f21409c59f29e254cfd1
apple <apple@MacBook-Pro.local>
2025-09-24 16:32:20 +0530

    otp

--- src/models/tags.model.ts
+++ src/models/tags.model.ts
@@ -2,13 +2,14 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
 import { BusinessIndustry } from 'src/business/model/businessIndustry.model';
 import { Category } from './contentCategory.model';
+import { BusinessCategory } from 'src/business/model/businessCategory.model';
 
 @Schema({ timestamps: true })
 export class Tag extends Document {
   @Prop()
   title: string;
 
-  @Prop({ required: true, enum: [BusinessIndustry.name, Category.name] })
+  @Prop({ required: true, enum: [BusinessCategory.name, Category.name] })
   relatedTo: string;
 
   @Prop({ refPath: 'relatedTo' })
